d707b5f5929872e966dc9ecce94d9cf8e48a7100
郑文浩 <shenmishajing@Gmail.com>
2018-08-23 20:57:49 +0800

    update 32.longest-valid-parentheses.cpp

---
+++ 32.longest-valid-parentheses.cpp
@@ -0,0 +1,48 @@
+/*
+ * [32] Longest Valid Parentheses
+ *
+ * https://leetcode-cn.com/problems/longest-valid-parentheses/description/
+ *
+ * algorithms
+ * Hard (22.04%)
+ * Total Accepted:    2.2K
+ * Total Submissions: 9.5K
+ * Testcase Example:  '"(()"'
+ *
+ * 给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+ *
+ * 示例 1:
+ *
+ * 输入: "(()"
+ * 输出: 2
+ * 解释: 最长有效括号子串为 "()"
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: ")()())"
+ * 输出: 4
+ * 解释: 最长有效括号子串为 "()()"
+ *
+ *
+ */
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> stk;
+        stk.push(-1);
+        int maxL=0;
+        for(int i=0;i<s.size();i++)
+        {
+            int t=stk.top();
+            if(t!=-1&&s[i]==')'&&s[t]=='(')
+            {
+                stk.pop();
+                maxL=max(maxL,i-stk.top());
+            }
+            else
+                stk.push(i);
+        }
+        return maxL;
+    }
+};
